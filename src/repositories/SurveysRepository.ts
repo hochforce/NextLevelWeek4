c4e177d5ffb75cdf17c92c1e1bc93023bc624edd
hochforce <hugohoch2013@gmail.com>
2021-02-24 21:01:09 -0300

    adicionando Surveys bd

---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,9 @@
+import { Repository, EntityRepository } from "typeorm"
+import { Survey } from "../models/survey";
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository<Survey> {
+
+}
+
+export { SurveysRepository };
